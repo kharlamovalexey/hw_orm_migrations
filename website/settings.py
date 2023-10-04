e0741908c6693bbd472ecb1e5002162c15a0026a
kharlamovalexey <kharlamovalexey@gmail.com>
2023-10-04 18:27:23 +0300

    models.py, views.py update

--- website/settings.py
+++ website/settings.py
@@ -73,9 +73,11 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_orm_migrations',
+        'NAME': 'netology',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'pgs',
     }
 }
 
